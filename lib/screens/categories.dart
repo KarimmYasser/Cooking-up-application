4d600691dbee405a3ce7157952715c7b861cf69a
KarimmYasser <Karimmyasserr@gmail.com>
2024-09-09 04:28:12 +0300

    ✨ animate Categories Screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -73,9 +73,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             )
         ],
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(
-          top: 100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.decelerate,
+          ),
         ),
         child: child,
       ),
